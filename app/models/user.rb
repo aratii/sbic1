a0edac67f17a0ab183e144abc3b3704f3e93c662
Arati Gurung <justified_rt@yahoo.com>
2012-01-10 19:55:05 +0000

    record species amended

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,36 @@
 class User < ActiveRecord::Base
+
+   attr_accessor :virtualPW
+   attr_accessible :name, :virtualPW
+
+  before_save :encrypt_password
+
+  def has_password?(submitted_password)
+    password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(name, submitted_password)
+    user = find_by_name(name)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+
+  private
+
+    def encrypt_password
+      self.salt = make_salt unless has_password?(virtualPW)
+      self.password = encrypt(virtualPW)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = { :name => "Example User", :virtualPW => "foobar"}
+  end
+  
+  describe "password encryption" do
+
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    
+    describe "has_password? method" do
+
+      it "should be true if the passwords match" do
+        @user.has_password?(@attr[:virtualPW]).should be_true
+      end    
+
+      it "should be false if the passwords don't match" do
+        @user.has_password?("invalid").should be_false
+      end 
+    end
+
+    describe "authenticate method" do
+
+      it "should return nil on name/password mismatch" do
+        wrong_password_user = User.authenticate(@attr[:name], "wrongpass")
+        wrong_password_user.should be_nil
+      end
+
+      it "should return the user on name/password match" do
+        matching_user = User.authenticate(@attr[:name], @attr[:virtualPW])
+        matching_user.should == @user
+      end
+    end
+  end
 end
